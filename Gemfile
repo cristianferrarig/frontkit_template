28fd2af98a764ffd8f63551d9f1ebbcb4321a07c
Cristian Ferrari <cristianferrarig@gmail.com>
2014-10-09 23:02:03 -0300

    Some little changes.

--- Gemfile
+++ Gemfile
@@ -1,20 +1,17 @@
-  # If you have OpenSSL installed, we recommend updating
+# If you have OpenSSL installed, we recommend updating
 # the following line to use "https"
-source 'http://rubygems.org'
+source "http://rubygems.org"
 
-gem "middleman-core", "~> 3.2.0"
+# gem "middleman-frontkit", "~> 0.0.1"
+gem "middleman-frontkit", "~> 0.0.1", github: "cristianferrarig/middleman-frontkit"
 
-gem 'middleman-sprockets', '~> 3.3.3'
-gem "middleman-livereload", "~> 3.1.0"
-# gem 'middleman-smusher', '~> 3.0.0'
-# gem "middleman-blog", "~> 3.4.1"
+# gem "middleman-blog",       "~> 3.4.1"
 
-gem 'middleman-autoprefixer', '~> 1.1.2'
-gem 'font-awesome-middleman', '~> 4.1.0'
+# For faster file watcher updates on Windows:
+gem "wdm", "~> 0.1.0", platforms: [:mswin, :mingw]
 
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw] # For faster file watcher updates on Windows:
-gem "builder", "~> 3.0" # For feed.xml.builder
+# Windows does not come with time zone data
+gem "tzinfo-data", platforms: [:mswin, :mingw]
 
-gem 'haml', '~> 4.0.5'
-gem 'sass', '~> 3.3.6'
-gem 'uglifier', '~> 2.5.0'
+# For feed.xml.builder
+gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -2,6 +2,8 @@
 # Page options, layouts, aliases and proxies
 ###
 
+# set :index_file, "index.html"
+
 # Per-page layout changes:
 #
 # With no layout
@@ -9,6 +11,7 @@
 #
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
+# page "/path/to/any/*", :layout => ":otherlayout"
 #
 # A path which all have the same layout
 # with_layout :admin do
@@ -27,32 +30,24 @@
 ###
 
 # Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+activate :automatic_image_sizes
 
+# Automatic alt attribute on image_tag helper
+activate :automatic_alt_tags
+
+# Automatically add vendor prefixes to CSS rules in stylesheets.
 # The extension has 2 optionally configurable fields:
 # https://github.com/ai/autoprefixer#browsers
 # activate :autoprefixer do |config|
 #   config.browsers = ['last 2 versions', 'Explorer >= 9']
 #   config.cascade  = false
 # end
-
-# Reload the browser automatically whenever files change
-activate :livereload
-# The extension supports a number of options that can be given to the activate statement.
-#activate :livereload do |config|
-#  config.apply_js_live  = false
-#  config.apply_css_live = false
-#end
+activate :autoprefixer
 
 # Middleman provides the Directory Indexes extension to tell Middleman
 # to create a folder for each .html file and place the built template file
 # as the index of that folder.
 activate :directory_indexes
-
-# set :index_file, "principal.html"
-
-# Automatically add vendor prefixes to CSS rules in stylesheets.
-activate :autoprefixer
 
 
 
@@ -70,28 +65,64 @@ activate :autoprefixer
 
 
 ###
-# Project configuration
+# Haml
 ###
 
-set :css_dir,    'assets/stylesheets'
-set :js_dir,     'assets/javascripts'
-set :images_dir, 'assets/images'
-set :fonts_dir,  'assets/fonts'
+## Haml to output unindented text
 
-# set :helpers_dir,  ''
-# set :layouts_dir,  'layouts'
-# set :partials_dir, ''
-# set :source,       'source'
+# CodeRay syntax highlighting in Haml
+# First: gem install haml-coderay
+# require 'haml-coderay'
+
+# CoffeeScript filters in Haml
+# First: gem install coffee-filter
+# require 'coffee-filter'
+
+
+
+###
+# Project configuration
+###
 
 ready do
   # if you have assets in directories other than your :js_dir or :css_dir,
   # you can make them importable by adding them to your Sprockets import path.
   sprockets.append_path '/vendor/assets'
   # Sprockets supports Bower, so you can add your Bower components path directly:
-  sprockets.append_path File.join root, '/vendor/bower'
+  sprockets.append_path File.join root, 'vendor/bower'
 end
 
+# for physical directories at development mode
+set :css_dir,    'assets/stylesheets'
+set :js_dir,     'assets/javascripts'
+set :images_dir, 'assets/images'
+set :fonts_dir,  'assets/fonts'
+
+
+# set :source,       'source'
+# set :build_dir,    'build'
+
+# set :data_dir,     'data'
+# set :helpers_dir,  'helpers'
+
+# set :layout,       :_auto_layout
+# set :layouts_dir,  'layouts'
+# set :partials_dir, ''
+
+# set :markdown_engine, :kramdown
+# set :relative_links,  false
+
+# set :http_prefix, '/'
+
 configure :development do
+  # Reload the browser automatically whenever files change
+  # The extension supports a number of options that can be given to the activate statement.
+  # activate :livereload do |config|
+  #   config.apply_js_live  = false
+  #   config.apply_css_live = false
+  # end
+  activate :livereload
+
   set :debug_assets, true
 end
 
@@ -101,24 +132,38 @@ configure :build do
   # Ignoring files
   # ignore 'javascripts/lib/*'
 
+  # Optimize images on build
+  activate :imageoptim
+
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  # Minify HTML files
+  activate :minify_html
 
   # Enable cache buster
-  # activate :asset_hash
-  # activate :cache_buster
+  activate :asset_hash
+  activate :cache_buster
 
   # Use relative URLs
   # activate :relative_assets
 
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
+  # activate :asset_host
+  # set :asset_host, "http://assets1.example.com"
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true # default: false
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
